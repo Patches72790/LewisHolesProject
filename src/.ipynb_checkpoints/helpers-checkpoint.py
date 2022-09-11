56c540a40c82ef4cdef9d615185acd6f6597504e
Patrick <patches72790@gmail.com>
2022-09-11 12:46:57 -0500

    added data and src code for lewis holes

---
+++ src/.ipynb_checkpoints/helpers-checkpoint.py
@@ -0,0 +1,25 @@
+
+# the columns 
+columns = [
+'Number',
+    'Block Type',
+    'Aylward #',
+    'Aylward Notes',
+    'Day Recorded',
+    'Lewis Type',
+    'Slant',
+    'Hole Depth',	
+    'Hole Length at Base',
+    'Hole Width',
+    'Contact Surface (cm^2)',
+    'Angle of Slant',
+    'Block Material',
+    'Block Width',
+    'Block Depth',
+    'Block Height'
+    'Volume, Block',
+    'Published/Measured',
+    'Notes on Volume/Shape',
+    'More notes',
+    'Mass of Block',
+]
---
+++ src/main.py
@@ -0,0 +1,7 @@
+import pandas
+
+def main():
+    print("hello")
+
+if __name__ == "__main__":
+    main()
