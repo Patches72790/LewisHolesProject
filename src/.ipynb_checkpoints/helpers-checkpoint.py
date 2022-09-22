5479fab0662343a7e9cc43f389c2ac4b9a6e9915
Patrick <patches72790@gmail.com>
2022-09-21 22:25:28 -0500

    added changes for xlsx version of samothrace data

--- src/.ipynb_checkpoints/helpers-checkpoint.py
+++ src/.ipynb_checkpoints/helpers-checkpoint.py
@@ -1,25 +1,43 @@
 
-# the columns 
-columns = [
-'Number',
+SAMOTHRACE_SHEET_NAMES = [
+    'Altar Court',
+    'Hall of Coral Dancers',
+    'Heiron',
+    'Milesian Lady',
+    'Miscellaneous',
+    'Phillip V',
+    'Phillip and Alexander',
+    'Propylon',
+    'Rotunda',
+    'Stoa'
+]
+
+COLUMN_NAMES = [
+#Numerical Entries:
+    'Slant',
+    'Hole Depth',    
+    'Hole Length at Base',
+    'Hole Width',
+
+    'Contact Surface (cm^2)',
+    'Angle of Slant',
+
+    'Block Width',
+    'Block Depth',
+    'Block Height'
+    'Volume, Block',
+    'Mass of Block',
+
+#Metadata Entries:
+    'ID', # My suggestion to uniquely identify each row
+    'Number',
     'Block Type',
     'Aylward #',
     'Aylward Notes',
     'Day Recorded',
     'Lewis Type',
-    'Slant',
-    'Hole Depth',	
-    'Hole Length at Base',
-    'Hole Width',
-    'Contact Surface (cm^2)',
-    'Angle of Slant',
     'Block Material',
-    'Block Width',
-    'Block Depth',
-    'Block Height'
-    'Volume, Block',
     'Published/Measured',
     'Notes on Volume/Shape',
-    'More notes',
-    'Mass of Block',
+    'More notes', #an addition I’ve made for certain columns without names
 ]
